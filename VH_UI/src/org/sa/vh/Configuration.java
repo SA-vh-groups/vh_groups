c066c43ea3ddc4bad388d6496bde2f8997554051
Salister <Salister@Salister-PC>
2014-07-16 01:55:05 +0900

    增加版本情報

---
+++ VH_UI/src/org/sa/vh/Configuration.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sa.vh;
+
+/**
+ *
+ * @author Salister
+ */
+public class Configuration {
+
+    private final static String VERSION = "0.0.0.1";
+
+    static {
+
+    }
+
+}
